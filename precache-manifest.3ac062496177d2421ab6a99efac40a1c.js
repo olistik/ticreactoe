c767bd577534391aa2a1a49ef12a8d1bd0a98417
olistik <root@olisti.co>
2019-08-23 10:39:48 +0200

    Updates to latest dependencies.

---
+++ precache-manifest.3ac062496177d2421ab6a99efac40a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff0e66d35d2ebdc2e4f54bd39404ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65903d099d8c9dc0e98b",
+    "url": "/static/css/main.16b74a6d.chunk.css"
+  },
+  {
+    "revision": "3edbf37882480a83ca69",
+    "url": "/static/js/2.676e9bb6.chunk.js"
+  },
+  {
+    "revision": "65903d099d8c9dc0e98b",
+    "url": "/static/js/main.7e93ee78.chunk.js"
+  },
+  {
+    "revision": "b63595000707be2f5602",
+    "url": "/static/js/runtime~main.22a5c0f9.js"
+  }
+]);
